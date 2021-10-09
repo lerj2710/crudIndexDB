0299881bebf5b1056ab80820313f845baed1bf35
luis rodriguez <kike.lerj2710@gmail.com>
2021-10-09 16:50:01 -0500

    se agregaron los object a indexDB

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -1,20 +1,23 @@
 (function(){
     let DB;
-    
     const formulario = document.querySelector('#formulario');
 
     document.addEventListener('DOMContentLoaded',()=>{
+    
         conectarDB();
-        formulario.addEventListener('submit', validarCliente)
+
+    formulario.addEventListener('submit', validarCliente);
     });
 
     function conectarDB(){
         const abrirConexion = window.indexedDB.open('crm', 1);
+    
         abrirConexion.onerror = ()=>{
             console.log('hubo un error');
         };
-        abrirConexion.onsuccess = ()=>{
-            console.log('Creada DB');
+      
+         abrirConexion.onsuccess = ()=>{
+            DB = abrirConexion.result;
         };
     };
 
@@ -30,13 +33,44 @@
             imprimirAlert('todos los campos son obligatorios', 'error');
             return;
         }
+
+        //crear el objecto principal donde se va llenar los formulaio
+        const cliente = {
+            nombreInput,
+            emailInput,
+            telefonoInput,
+            empresaInput,
+        }
+
+        cliente.id = Date.now();
+
+        crearNuevoCliente(cliente);
+       
+    }
+    function crearNuevoCliente(cliente) {
+
+        const transaction = DB.transaction(['crm'], 'readwrite');
+            const objectStore = transaction.objectStore('crm');
+            objectStore.add(cliente);
+
+            // si hay un error
+            transaction.onerror = ()=>{
+                imprimirAlert('hubo un error');
+            };
+         
+            transaction.oncomplete = ()=>{
+                imprimirAlert('Cliente creado correctamente');
+                setTimeout(() => {
+                    window.location.href = 'index.html'
+                }, 3000);
+            }
     }
     function imprimirAlert(mensaje, tipo) {
         //crear el mensaje
         const alerta = document.querySelector('.alerta');
         if (!alerta) {
-            
             const divMensaje = document.createElement('div');
+            
             divMensaje.classList.add('px-4','rounded', 'max-w-lg', 'mt-6', 'text-center', 'border','alerta');
     
             if (tipo === 'error') {
